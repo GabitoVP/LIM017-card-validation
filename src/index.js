ecc343bd568e1e011d7ce91f0efbd26209bc3539
Gabriela Victorio <lucerovp16@gmail.com>
2022-02-09 22:15:13 -0500

    mi segundo commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 import validator from './validator.js';
 
-console.log(validator);
+let button=document.getElementById("btn");
+let card=document.getElementById("number");
+//let card2=document.getElementById("number");
+let validMessage=document.getElementById("Message");
+button.addEventListener("click",clickButton);
+//document.getElementById("number").addEventListener("input", maxLengthCheck);
+document.getElementById("number").addEventListener("keypress", isNumeric);
+//function maxLengthCheck(object) {
+  //if (object.value.length > object.max.length)
+    //object.value = object.value.slice(0, object.max.length);
+//}
+
+function isNumeric (evt) {
+  let theEvent = evt || window.event;
+  let key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode (key);
+  let regex = /[0-9]|\./;
+  if ( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+
+function clickButton(){
+  card=document.getElementById("number").value;
+  if (card.length < 10 || card.length > 16){
+    alert("El número de tarjeta tiene que ser entre 10 y 16 dígitos");
+  }else{
+    //card2=document.getElementById("number").value;
+    //card2=validator.maskify(card2);
+    //document.getElementById("number2").value=card2;
+    if (validator.isValid(card)==true){
+      //card2=document.getElementById("number").value;
+      card=validator.maskify(card);
+      document.getElementById("number2").value=card;
+      validMessage.innerHTML="Tarjeta válida";
+    }
+    else{
+      validMessage.innerHTML="Tarjeta válida";
+    }
+  
+  }
+}
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,46 @@
 const validator = {
-  // ...
-};
+  isValid: function (creditCardNumber){
+    let array =Array.from(creditCardNumber);
+    let array2=[];
+    let sumNum=0;
+    for(let i=0;i<array.length;i++){
+      array2.push(parseInt(array[i]));
+    }
+    array2.reverse();
+    for (let i=0; i<array2.length; i++){
+      if((i+1)%2==0){
+        array2[i]*=2;
+        if(array2[i]>=10){
+          array2[i]-=9;
+        }
+      sumNum+=array2[i];
+      }
+      else{
+      sumNum+=array2[i];
+      } 
+    }
+  if(sumNum%10==0 && sumNum!=0){
+    return true;
+  }
+  else{
+    return false;
+  }
 
+},
+  maskify: function(creditCardNumber){
+    let hideNum = [];
+    for(let i = 0; i < creditCardNumber.length; i++){
+      if(i < creditCardNumber.length-4){
+        hideNum.push("#");
+      }else{
+        hideNum.push(creditCardNumber[i]);
+        }
+    }
+    return hideNum.join("");
+  
+  }
+
+  
+};
+validator.isValid(56)
 export default validator;
