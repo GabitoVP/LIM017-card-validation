4e126790ec86d2b95a2b4839a0563e6d2c7c4ce3
Gabriela Victorio <lucerovp16@gmail.com>
2022-02-21 10:02:36 -0500

    mi octavo commit

--- src/index.js
+++ src/index.js
@@ -8,20 +8,20 @@ button.addEventListener("click",clickButton);
 
 function clickButton(){
   let card=document.getElementById("number").value;
-  let card2;
+  let maskyCreditNumber;
   if (card.length > 0){
     if (card.length > 9 && card.length <= 16){
       validCreditCardDigit.innerHTML="";
       if (validator.isValid(card)==true){
         validMessage.innerHTML="Tarjeta válida";
         validMessage.style.color="blue";
-        card2=validator.maskify(card);
-        document.getElementById("number2").value=card2;
+        maskyCreditNumber=validator.maskify(card);
+        document.getElementById("number2").value=maskyCreditNumber;
       }
       else{
         validMessage.innerHTML="Tarjeta inválida";
         validMessage.style.color="red";
-        card2=document.getElementById("number2").value="";
+        maskyCreditNumber=document.getElementById("number2").value="";
       }
 
       if(validator.getIssuer(card)=="Visa"){
